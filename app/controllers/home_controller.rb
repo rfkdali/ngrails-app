cbd973c3264bc2c9953043fd66f0a4f994b93b74
rfkdali <rfkdali@gmail.com>
2016-04-24 17:59:57 +0100

    Parse json file to replace unquoted attributes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
 
   def conditions
-    data = File.read("#{Rails.root}/public/conditions.json")
+    data = File.read("#{Rails.root}/public/conditions.json").gsub!(/(['"])?([a-zA-Z0-9_]+)(['"])?:/, '"\2":')
     render json: data
   end
 end
